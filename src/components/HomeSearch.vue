5cd7d2ec62b47c0d794815e3c1449842d3d4f49b
moisessolares <moisessolaressantos@gmail.com>
2023-11-22 14:40:35 -0300

    Add v-menu, profilepic and notifications button

--- src/components/HomeSearch.vue
+++ src/components/HomeSearch.vue
@@ -140,7 +140,56 @@
             class="ma-2 pa-2 rounded-lg size-main background-main"
             color="#121212"
           >
-            <main class="d-flex flex-column pa-0"></main>
+            <header>
+              <v-row class="pa-3">
+                <v-col class="d-flex pt-0 mt-2">
+                  <v-col>
+                    <v-btn color="#121212" size="30" class="ml-2 rounded-circle"
+                      ><v-icon size="x-large" icon="mdi-chevron-left"></v-icon>
+                    </v-btn>
+                    <v-btn color="#121212" size="30" class="ml-3 rounded-circle"
+                      ><v-icon size="x-large" icon="mdi-chevron-right"></v-icon>
+                    </v-btn>
+                  </v-col>
+                  <v-col class="d-flex align-center justify-end">
+                    <v-btn
+                      size="30"
+                      color="#0F0F0F"
+                      class="rounded-circle mr-3"
+                    >
+                      <v-icon class="text-cor" icon="mdi-bell-outline"></v-icon>
+                    </v-btn>
+
+                    <v-menu>
+                      <template v-slot:activator="{ props }">
+                        <v-btn
+                          color="#0F0F0F"
+                          v-bind="props"
+                          size="55"
+                          class="mt-1 rounded-circle"
+                        >
+                          <v-avatar size="50">
+                            <img
+                              width="50"
+                              src="../assets/img/profile-pic.jpeg"
+                            />
+                          </v-avatar>
+                        </v-btn>
+                      </template>
+                      <v-list bg-color="#121212" class="mt-5">
+                        <v-list-item class="pr-16 mr-16"> Account </v-list-item>
+                        <v-list-item> Profile </v-list-item>
+                        <v-list-item>Support</v-list-item>
+                        <v-list-item>Download</v-list-item>
+                        <v-list-item>Settings</v-list-item>
+                        <v-divider></v-divider>
+                        <v-list-item> Logout </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </v-col>
+                </v-col>
+              </v-row>
+            </header>
           </v-card>
         </v-col>
       </v-row>
