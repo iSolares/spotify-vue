fcb605460ff31561a0928554c496a19a23f13853
Moisés Solares Santos <moisessolaressantos@gmail.com>
2023-12-12 14:59:37 -0300

    add new component for login

---
+++ src/components/Login-Component/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <div class="bg-green h-100 largura"> 
+            <p class="d-flex justify-center mt-16 pt-16 ml-6 text-h3">Join now to start enjoying your favorite music</p>
+        </div>
+    </v-app>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.largura {
+    width: 40%;
+}
+</style>
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,25 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import Default from "@/layouts/default/Default.vue"
+import Login from "@/views/Login.vue"
 
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: Default,
     children: [
       {
-        path: '',
+        path: 'Home',
         name: 'Home',
-
         component: Home
       },
     ],
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login 
   },
 ]
 
