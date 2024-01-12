c7853177ebb82e7b784d4647c2fb1cd2d4370f5b
Moisés Solares Santos <moisessolaressantos@gmail.com>
2024-01-12 16:18:19 -0300

    add initial code to father component

--- src/components/Home-component/Home.vue
+++ src/components/Home-component/Home.vue
@@ -11,6 +11,83 @@
 <script lang="ts" setup>
 import SideComponent from "@/components/Side-Components/index.vue";
 import MainComponent from "@/components/Main-Component/index.vue";
+// import { ref, watchEffect } from "vue";
+// import axios from "axios";
+// import exp from "constants";
+
+// const accessToken = ref("");
+// const playlist = ref([]);
+// const clientId: string = "fed9d0d38d384a438545f78d75e0e5a7";
+// const clientSecret: string = "fd18d068c9b1484c80f70c6e3f0680ed";
+// const refreshToken = ref("")
+// const expiresIn = ref("")
+
+// const getToken = async () => {
+//   let params = new URLSearchParams(document.location.search);
+//   let code: string = params.get("code");
+
+//   let body = {
+//     grant_type: "authorization_code",
+//     code,
+//     redirect_uri: "http://localhost:3000/home",
+//     client_id: clientId,
+//     client_secret: clientSecret,
+//   };
+
+//   const res = await axios({
+//     method: "POST",
+//     url: "https://accounts.spotify.com/api/token",
+//     data: new URLSearchParams(body).toString(),
+//     headers: {
+//       Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//   });
+//   accessToken.value = res.data.access_token;
+//   refreshToken.value = res.data.refresh_token
+//   setTimeout(() => {
+//     getRefreshToken()
+//   },expiresIn.value = res.data.expires_in)
+// };
+
+// const getRefreshToken = async () => {
+//   let body = {
+//     grant_type: "refresh_token",
+//     refresh_token: refreshToken.value,
+//   };
+  
+//   const res = await axios({
+//     method: "POST",
+//     url: "https://accounts.spotify.com/api/token",
+//     data: new URLSearchParams(body).toString(),
+//     headers: {
+//       Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//   });
+//   accessToken.value = res.data.access_token
+//   expiresIn.value = res.data.expires_in
+//   setTimeout(() => {
+//     getRefreshToken()
+//   },expiresIn.value = res.data.expires_in)
+// } 
+
+// const getUserPlaylist = async () => {
+//   const res = await axios({
+//     method: "GET",
+//     url: "https://api.spotify.com/v1/me/playlists",
+//     headers: {
+//       Authorization: `Bearer ${accessToken.value}`,
+//     },
+//   });
+//   playlist.value = res.data.items.filter((_, index) => index <= 7);
+// };
+
+// watchEffect(async () => {
+//   await getToken();
+//   await getUserPlaylist();
+//   await getRefreshToken()
+// });
 </script>
 
 <style>
