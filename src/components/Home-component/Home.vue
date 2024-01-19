5e222095c6a27472f650f9cbca324ce618a86a30
Moisés Solares Santos <moisessolaressantos@gmail.com>
2024-01-19 15:28:34 -0300

    Unified code and use vue useQuery to call functions

--- src/components/Home-component/Home.vue
+++ src/components/Home-component/Home.vue
@@ -1,93 +1,125 @@
 <template>
-
   <div class="h-100 bg-black">
-    <v-row class="h-25">
-        <SideComponent />
-      <MainComponent />
-    </v-row>
+    <div v-if="isLoading && isLoadingProfilepic">Carrgando...</div>
+    <div v-else>
+        <v-row class="h-25">
+        <SideComponent :playlist="playlist" />
+        <MainComponent
+          :playlist="playlist"
+          :profilePic="profilePic"
+          :recents="recents"
+        />
+      </v-row>
+      </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import SideComponent from "@/components/Side-Components/index.vue";
 import MainComponent from "@/components/Main-Component/index.vue";
-// import { ref, watchEffect } from "vue";
-// import axios from "axios";
-// import exp from "constants";
+import { ref, watchEffect } from "vue";
+import axios from "axios";
+import { useQuery } from "@tanstack/vue-query";
 
-// const accessToken = ref("");
-// const playlist = ref([]);
-// const clientId: string = "fed9d0d38d384a438545f78d75e0e5a7";
-// const clientSecret: string = "fd18d068c9b1484c80f70c6e3f0680ed";
-// const refreshToken = ref("")
-// const expiresIn = ref("")
+const accessToken = ref("");
+const playlist = ref([]);
+const clientId: string = "fed9d0d38d384a438545f78d75e0e5a7";
+const clientSecret: string = "fd18d068c9b1484c80f70c6e3f0680ed";
+const refreshToken = ref("");
+const expiresIn = ref("");
+const recents = ref();
 
-// const getToken = async () => {
-//   let params = new URLSearchParams(document.location.search);
-//   let code: string = params.get("code");
+const { data, isLoading } = useQuery({
+  queryKey: ["getSpotifyPlaylist"],
+  queryFn: async () => getUserPlaylist()
+});
 
-//   let body = {
-//     grant_type: "authorization_code",
-//     code,
-//     redirect_uri: "http://localhost:3000/home",
-//     client_id: clientId,
-//     client_secret: clientSecret,
-//   };
+const { data:profilePic, isLoading: isLoadingProfilepic } = useQuery({
+  queryKey: ["getSpotifyProfilePic"],
+  queryFn: async () => getProfilePic()
+});
 
-//   const res = await axios({
-//     method: "POST",
-//     url: "https://accounts.spotify.com/api/token",
-//     data: new URLSearchParams(body).toString(),
-//     headers: {
-//       Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//   });
-//   accessToken.value = res.data.access_token;
-//   refreshToken.value = res.data.refresh_token
-//   setTimeout(() => {
-//     getRefreshToken()
-//   },expiresIn.value = res.data.expires_in)
-// };
+console.log(data);
 
-// const getRefreshToken = async () => {
-//   let body = {
-//     grant_type: "refresh_token",
-//     refresh_token: refreshToken.value,
-//   };
-  
-//   const res = await axios({
-//     method: "POST",
-//     url: "https://accounts.spotify.com/api/token",
-//     data: new URLSearchParams(body).toString(),
-//     headers: {
-//       Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//   });
-//   accessToken.value = res.data.access_token
-//   expiresIn.value = res.data.expires_in
-//   setTimeout(() => {
-//     getRefreshToken()
-//   },expiresIn.value = res.data.expires_in)
-// } 
+const getToken = async () => {
+  let params = new URLSearchParams(document.location.search);
+  let code: string | null = params.get("code");
 
-// const getUserPlaylist = async () => {
-//   const res = await axios({
-//     method: "GET",
-//     url: "https://api.spotify.com/v1/me/playlists",
-//     headers: {
-//       Authorization: `Bearer ${accessToken.value}`,
-//     },
-//   });
-//   playlist.value = res.data.items.filter((_, index) => index <= 7);
-// };
+  let body = {
+    grant_type: "authorization_code",
+    code,
+    redirect_uri: "http://localhost:3000/home",
+    client_id: clientId,
+    client_secret: clientSecret,
+  };
 
-// watchEffect(async () => {
-//   await getToken();
-//   await getUserPlaylist();
-//   await getRefreshToken()
-// });
+  const res = await axios({
+    method: "POST",
+    url: "https://accounts.spotify.com/api/token",
+    data: new URLSearchParams(body).toString(),
+    headers: {
+      Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  accessToken.value = res.data.access_token;
+  refreshToken.value = res.data.refresh_token;
+  setTimeout(() => {
+    getRefreshToken();
+    localStorage.setItem("userToken", refreshToken.value);
+  }, (expiresIn.value = res.data.expires_in));
+};
+
+const getRefreshToken = async () => {
+  let body = {
+    grant_type: "refresh_token",
+    refresh_token: refreshToken.value,
+  };
+
+  const res = await axios({
+    method: "POST",
+    url: "https://accounts.spotify.com/api/token",
+    data: new URLSearchParams(body).toString(),
+    headers: {
+      Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  accessToken.value = res.data.access_token;
+  expiresIn.value = res.data.expires_in;
+  refreshToken.value = res.data.refresh_token;
+  setTimeout(() => {
+    getRefreshToken();
+  }, (expiresIn.value = res.data.expires_in));
+};
+
+const getUserPlaylist = async () => {
+  const res = await axios({
+    method: "GET",
+    url: "https://api.spotify.com/v1/me/playlists",
+    headers: {
+      Authorization: `Bearer ${accessToken.value}`,
+    },
+  });
+  playlist.value = res.data.items.filter((_, index) => index <= 7);
+  recents.value = res.data.items.filter((_, index) => index <= 5);
+  return res.data;
+};
+
+const getProfilePic = async () => {
+  const res = await axios({
+    method: "GET",
+    url: "https://api.spotify.com/v1/me",
+    headers: {
+      Authorization: `Bearer ${accessToken.value}`,
+    },
+  });
+  return res.data.images[0].url;
+};
+
+watchEffect(async () => {
+  await getToken();
+});
 </script>
 
 <style>
